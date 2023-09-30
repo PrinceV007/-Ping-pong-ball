bc9150d09faf7dd8d24ae4b3c57a652aae042576
PrinceV007 <143865246+PrinceV007@users.noreply.github.com>
2023-09-30 23:33:13 +0530

    Created paddle function

--- ping_pong_ball.py
+++ ping_pong_ball.py
@@ -1,28 +1,28 @@
 import pygame
-import os
-#Setup Display
+import sys
 pygame.init()
-WIDTH, HEIGHT=800, 500
-win=pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Ping Pong Ball Game")
+width = 800
+height = 400
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Ping Pong Ball")
+clock = pygame.time.Clock()
+paddle_width = 10
+paddle_height = 60
+paddle1 = pygame.Rect(50, height // 2 - paddle_height // 2, paddle_width, paddle_height)
+paddle2 = pygame.Rect(width - 50 - paddle_width, height // 2 - paddle_height // 2, paddle_width, paddle_height)
+ball = pygame.Rect(width // 2 - 10, height // 2 - 10, 20, 20)
+ball_speed_x = 3
+ball_speed_y = 3
+paddle_speed = 5
 
-#colour
-WHITE=(255,255,255)
-#Setup Game
-FPS=60
-clock=pygame.time.Clock()
-run=True
-while run:
-    clock.tick(FPS)
+def move_paddle1(up, down):
+    if up:
+        paddle1.y -= paddle_speed
+    if down:
+        paddle1.y += paddle_speed
 
-    win.fill(WHITE)
-    pygame.display.update()
-
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            run=False
-        if event.type==pygame.MOUSEBUTTONDOWN:
-            pos=pygame.mouse.get_pos()
-            print(pos)
-
-pygame.quit()
+def move_paddle2(up, down):
+    if up:
+        paddle2.y -= paddle_speed
+    if down:
+        paddle2.y += paddle_speed
